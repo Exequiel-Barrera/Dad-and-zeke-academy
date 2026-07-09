c6d83a5be3d06f307f130e8da89db20005c680c7
Exequiel-Barrera <exequielbm86@gmail.com>
2026-07-09 12:37:00 +1200

    build home page with reusable components

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,19 +1,7 @@
-import Header from './Header.tsx'
-import StatsCard from './StatsCard.tsx'
-import MissionCard from './MissionCard.tsx'
-import NavigationCards from './NavigationCards.tsx'
+import Home from '../pages/Home.tsx'
 
 function App() {
-  return (
-    <main className="min-h-screen bg-sky-100 p-8">
-      <Header />
-      <div className="mx-auto mt-10 max-w-4xl space-y-8">
-        <StatsCard />
-        <MissionCard />
-        <NavigationCards />
-      </div>
-    </main>
-  )
+  return <Home />
 }
 
 export default App
---
+++ client/pages/Home.tsx
@@ -0,0 +1,19 @@
+import Header from '../components/Header.tsx'
+import StatsCard from '../components/StatsCard.tsx'
+import MissionCard from '../components/MissionCard.tsx'
+import NavigationCards from '../components/NavigationCards.tsx'
+
+function Home() {
+  return (
+    <main className="min-h-screen bg-sky-100 p-8">
+      <Header />
+      <div className="mx-auto mt-10 max-w-4xl space-y-8">
+        <StatsCard />
+        <MissionCard />
+        <NavigationCards />
+      </div>
+    </main>
+  )
+}
+
+export default Home
